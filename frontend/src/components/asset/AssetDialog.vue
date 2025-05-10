094edca4a86f84a08e0bf35bfe3c81018ea30f32
geekhall <174821300@qq.com>
2025-05-10 23:16:59 +0800

    chore: style

--- frontend/src/components/asset/AssetDialog.vue
+++ frontend/src/components/asset/AssetDialog.vue
@@ -2,144 +2,168 @@
   <el-dialog
     :title="mode === 'add' ? '新增资产' : '编辑资产'"
     v-model="dialogVisible"
-    width="50%"
+    width="65%"
     @close="handleClose"
+    class="asset-dialog"
   >
-    <el-form :model="form" label-width="100px" ref="formRef" :rules="rules">
-      <el-form-item label="ID" v-if="mode === 'edit'">
+    <el-form :model="form" label-width="100px" ref="formRef" :rules="rules" class="asset-form">
+      <el-form-item label="ID" v-if="mode === 'edit'" class="form-item-id">
         <el-input v-model="form.id" disabled></el-input>
       </el-form-item>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="资产名称" prop="name">
-            <el-input v-model="form.name"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="资产编号" prop="code">
-            <el-input v-model="form.code"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="资产序列号" prop="sn">
-            <el-input v-model="form.sn"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="资产类型" prop="type">
-            <el-select v-model="form.type" placeholder="请选择" style="width: 100%">
-              <el-option
-                v-for="item in assetTypes"
-                :key="item.name"
-                :label="item.name"
-                :value="item.name"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="资产型号" prop="model">
-            <el-input v-model="form.model"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="资产配置" prop="config">
-            <el-input v-model="form.config"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="资产IP" prop="ip">
-            <el-input v-model="form.ip"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="资产描述" prop="description">
-            <el-input v-model="form.description"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="资产提供商" prop="provider">
-            <el-input v-model="form.provider"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="所属部门" prop="departmentName">
-            <el-select v-model="form.departmentName" placeholder="请选择" style="width: 100%">
-              <el-option
-                v-for="item in departments"
-                :key="item.name"
-                :label="item.name"
-                :value="item.name"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="资产位置" prop="location">
-            <el-input v-model="form.location"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="状态" prop="status">
-            <el-select v-model="form.status" placeholder="请选择" style="width: 100%">
-              <el-option label="正常" value="正常"></el-option>
-              <el-option label="维修" value="维修"></el-option>
-              <el-option label="报废" value="报废"></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="使用状态" prop="useStatus">
-            <el-select v-model="form.useStatus" placeholder="请选择" style="width: 100%">
-              <el-option label="在用" value="在用"></el-option>
-              <el-option label="闲置" value="闲置"></el-option>
-              <el-option label="维修" value="维修"></el-option>
-              <el-option label="报废" value="报废"></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="购入时间" prop="purchaseDate">
-            <el-date-picker
-              v-model="form.purchaseDate"
-              type="date"
-              format="YYYY-MM-DD"
-              value-format="YYYY-MM-DD"
-              style="width: 100%"
-              placeholder="选择日期"
-            />
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="购买价格" prop="purchasePrice">
-            <el-input-number
-              v-model="form.purchasePrice"
-              :precision="2"
-              :step="100"
-              style="width: 100%"
-            ></el-input-number>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="数量" prop="count">
-            <el-input-number v-model="form.count" :min="1" style="width: 100%"></el-input-number>
-          </el-form-item>
-        </el-col>
-      </el-row>
+
+      <div class="form-section">
+        <div class="section-title">基本信息</div>
+        <el-row :gutter="24">
+          <el-col :span="8">
+            <el-form-item label="资产名称" prop="name">
+              <el-input v-model="form.name" placeholder="请输入资产名称"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="资产编号" prop="code">
+              <el-input v-model="form.code" placeholder="请输入资产编号"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="资产序列号" prop="sn">
+              <el-input v-model="form.sn" placeholder="请输入资产序列号"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="24">
+          <el-col :span="8">
+            <el-form-item label="资产类型" prop="type">
+              <el-select v-model="form.type" placeholder="请选择资产类型" style="width: 100%">
+                <el-option
+                  v-for="item in assetTypes"
+                  :key="item.name"
+                  :label="item.name"
+                  :value="item.name"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="资产型号" prop="model">
+              <el-input v-model="form.model" placeholder="请输入资产型号"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="资产配置" prop="config">
+              <el-input v-model="form.config" placeholder="请输入资产配置"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+
+      <div class="form-section">
+        <div class="section-title">网络信息</div>
+        <el-row :gutter="24">
+          <el-col :span="8">
+            <el-form-item label="资产IP" prop="ip">
+              <el-input v-model="form.ip" placeholder="请输入资产IP"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="16">
+            <el-form-item label="资产描述" prop="description">
+              <el-input v-model="form.description" placeholder="请输入资产描述"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+
+      <div class="form-section">
+        <div class="section-title">采购信息</div>
+        <el-row :gutter="24">
+          <el-col :span="8">
+            <el-form-item label="资产提供商" prop="provider">
+              <el-input v-model="form.provider" placeholder="请输入资产提供商"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="购入时间" prop="purchaseDate">
+              <el-date-picker
+                v-model="form.purchaseDate"
+                type="date"
+                format="YYYY-MM-DD"
+                value-format="YYYY-MM-DD"
+                style="width: 100%"
+                placeholder="选择购入时间"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="购买价格" prop="purchasePrice">
+              <el-input-number
+                v-model="form.purchasePrice"
+                :precision="2"
+                :step="100"
+                :min="0"
+                style="width: 100%"
+                placeholder="请输入购买价格"
+              ></el-input-number>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+
+      <div class="form-section">
+        <div class="section-title">使用信息</div>
+        <el-row :gutter="24">
+          <el-col :span="8">
+            <el-form-item label="所属部门" prop="departmentName">
+              <el-select
+                v-model="form.departmentName"
+                placeholder="请选择所属部门"
+                style="width: 100%"
+              >
+                <el-option
+                  v-for="item in departments"
+                  :key="item.name"
+                  :label="item.name"
+                  :value="item.name"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="资产位置" prop="location">
+              <el-input v-model="form.location" placeholder="请输入资产位置"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="数量" prop="count">
+              <el-input-number
+                v-model="form.count"
+                :min="1"
+                style="width: 100%"
+                placeholder="请输入数量"
+              ></el-input-number>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="24">
+          <el-col :span="8">
+            <el-form-item label="状态" prop="status">
+              <el-select v-model="form.status" placeholder="请选择状态" style="width: 100%">
+                <el-option label="正常" value="正常"></el-option>
+                <el-option label="维修" value="维修"></el-option>
+                <el-option label="报废" value="报废"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="使用状态" prop="useStatus">
+              <el-select v-model="form.useStatus" placeholder="请选择使用状态" style="width: 100%">
+                <el-option label="在用" value="在用"></el-option>
+                <el-option label="闲置" value="闲置"></el-option>
+                <el-option label="维修" value="维修"></el-option>
+                <el-option label="报废" value="报废"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
@@ -273,13 +297,69 @@ const handleSubmit = async () => {
 </script>
 
 <style scoped>
-.dialog-footer {
-  display: flex;
-  justify-content: flex-end;
-  gap: 10px;
+.asset-dialog {
+  :deep(.el-dialog__body) {
+    padding: 20px 30px;
+  }
+}
+
+.asset-form {
+  .form-item-id {
+    margin-bottom: 20px;
+  }
+}
+
+.form-section {
+  background-color: #f8f9fa;
+  border-radius: 8px;
+  padding: 20px;
+  margin-bottom: 24px;
+
+  .section-title {
+    font-size: 16px;
+    font-weight: 600;
+    color: #303133;
+    margin-bottom: 20px;
+    padding-left: 10px;
+    border-left: 4px solid #409eff;
+  }
+}
+
+:deep(.el-form-item) {
+  margin-bottom: 20px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  .el-form-item__label {
+    font-weight: 500;
+  }
 }
 
 :deep(.el-input-number) {
   width: 100%;
+}
+
+.dialog-footer {
+  display: flex;
+  justify-content: flex-end;
+  gap: 12px;
+  padding-top: 20px;
+}
+
+:deep(.el-input__wrapper),
+:deep(.el-select),
+:deep(.el-date-editor) {
+  box-shadow: none !important;
+  border: 1px solid #dcdfe6;
+
+  &:hover {
+    border-color: #409eff;
+  }
+
+  &.is-focus {
+    border-color: #409eff;
+  }
 }
 </style>
